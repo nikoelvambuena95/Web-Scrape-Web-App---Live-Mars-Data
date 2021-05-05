a8cc1a441e6d2e04aeace5ed71648a8b8926180f
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-05-04 18:09:33 -0700

    index.html file

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", planet_data = data)
 
 
 # Route that will trigger the scrape function
@@ -25,10 +25,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
+    mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
